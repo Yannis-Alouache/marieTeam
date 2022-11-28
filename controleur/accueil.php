bc8a0ad8de33f82f8e77655f264c7753a58474d3
RedCHK <redchk03@yahoo.com>
2022-11-28 17:19:03 +0100

    [+] add Footer

--- controleur/accueil.php
+++ controleur/accueil.php
@@ -5,7 +5,34 @@ if ( $_SERVER["SCRIPT_FILENAME"] == __FILE__ ){
 
 $titre = "Accueil";
 include "$racine/vue/header.html.php";
+echo "<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<br>
+<br>
+<br>
+
+<br>
+";
+
 //include "$racine/vue/accueil.html.php";
-//include "$racine/vue/footer.html.php";
+include "$racine/vue/footer.html.php";
 
 ?>

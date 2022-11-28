a3aa32b9830f2305c83af635d8f030daed06ffcb
Yannis-Alouache <alouacehyannis1@gmail.com>
2022-11-28 16:25:41 +0100

    [~] fixed typo && rm old accueil

--- controleur/accueil.php
+++ controleur/accueil.php
@@ -3,7 +3,7 @@ if ( $_SERVER["SCRIPT_FILENAME"] == __FILE__ ){
     $racine="..";
 }
 
-$titre = "Acceuil";
+$titre = "Accueil";
 include "$racine/vue/header.html.php";
 include "$racine/vue/acceuil.html.php";
 include "$racine/vue/footer.html.php";
--- controleur/controleurPrincipal.php
+++ controleur/controleurPrincipal.php
@@ -2,7 +2,7 @@
 
 function controleurPrincipal($action){
     $lesActions = array();
-    $lesActions["defaut"] = "acceuil.php";
+    $lesActions["defaut"] = "accueil.php";
     $lesActions["liste"] = "listeRestos.php";
     $lesActions["detail"] = "detailResto.php";
     $lesActions["connexion"] = "connexion.php";

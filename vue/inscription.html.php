e3048a87b5746107719515a63a9e46935b7b16f7
sio sio <sio@sio.lan>
2022-12-05 15:01:44 +0100

    [+] added vue for inscription && fixed connexion

--- vue/inscription.html.php
+++ vue/inscription.html.php
@@ -1,5 +1,4 @@
-    <div class="breadcumb-area bg-img bg-overlay">
-    </div>
+    <div class="breadcumb-area bg-img bg-overlay"></div>
     <?php
         if ($message != "") {
             echo "<div class='alert alert-danger' role='alert'>
@@ -9,18 +8,23 @@
     ?>
 
  
- <!-- ***** Contact Area Start ***** -->
     <div class="dorne-contact-area container" id="contact">
         <!-- Contact Form Area -->
         <div class="contact-form-area equal-height">
             <div class="contact-text">
-                <h4>Connexion</h4>
+                <h4>Inscription</h4>
             </div>
             <div class="contact-form">
-                <form action="./?action=connexion" method="POST">
+                <form action="./?action=inscription" method="POST">
                     <div class="row">
                         <div class="col-12">
                             <input type="email" name="mail" class="form-control" placeholder="Adresse Mail">
+                        </div>
+                        <div class="col-12">
+                            <input type="text" name="name" class="form-control" placeholder="Nom">
+                        </div>
+                        <div class="col-12">
+                            <input type="text" name="first_name" class="form-control" placeholder="Prenom">
                         </div>
                         <div class="col-12">
                             <input type="password" name="password" class="form-control" placeholder="Mot de passe">
